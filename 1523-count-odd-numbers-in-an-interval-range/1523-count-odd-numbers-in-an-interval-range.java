efa8f7db3be2c2321c63e084d59dc0b90ea1e895
Manasi-Ranjekar <manasi.ranjekar6@gmail.com>
2026-03-01 11:39:16 +0530

    Time: 0 ms (100%), Space: 42.3 MB (41.94%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
@@ -1,10 +1,15 @@
+//O(1) Complexity
 class Solution {
     public int countOdds(int low, int high) {
+        //calculate total numbers in the range
+        //low=3,high=7 total=7-3+1=5
         int total = high - low + 1;
-        
+        //if total is even then exactly half of them are odd
         if (total % 2 == 0) {
             return total / 2;
-        } else {
+        } 
+        else {
+            //if range starts form odd add 1 else add 0
             return total / 2 + (low % 2);
         }
     }
