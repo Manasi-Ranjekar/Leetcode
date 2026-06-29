a15a35af36fb6602724dcb5d00996ba1c2e07ae6
Manasi-Ranjekar <manasi.ranjekar6@gmail.com>
2026-06-29 13:25:21 +0530

    Time: 1 ms (26.43%), Space: 42 MB (99.92%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,15 +1,19 @@
 class Solution {
     public String toLowerCase(String s) {
+        //mutable(it can modify the same object instead of creating a new one every time)
         StringBuilder result=new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
+            //convert upper case characters to lower case using ASCII logic
             if(ch>='A'&&ch<='Z'){
+                //adds data to the end of the existing sequence of characters.
                 result.append((char)(ch+32));
             }
             else{
                 result.append(ch);
             }
         }
+        //return as String and not StringBuilder
         return result.toString();
     }
 }
