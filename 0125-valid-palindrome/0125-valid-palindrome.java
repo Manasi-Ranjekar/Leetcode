c8fd12cf24c0beb502247b979415df2eb3e4cd26
Manasi-Ranjekar <manasi.ranjekar6@gmail.com>
2026-06-29 13:17:54 +0530

    Time: 2 ms (99.23%), Space: 44.2 MB (84.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,37 @@
 class Solution {
+    //check if character is alphanumeric
+    boolean isAlphaNumeric(char ch){
+        return (ch>='A' && ch<='Z')||(ch>='a' && ch<='z')||(ch>='0' && ch<='9');
+    }
+    //convert upper case character to lower case
+    char toLower(char ch){
+        if(ch>='A' && ch<='Z'){
+            ch=(char)(ch+32);
+        }
+        return ch;
+    }
     public boolean isPalindrome(String s){
+        //initialize 2 pointers one at beginning and other at the end of string
         int left=0;
         int right=s.length()-1;
         while(left<right){
-            while(left<right&&!Character.isLetterOrDigit(s.charAt(left))){
+            //skip any character that is not a letter or digit
+            while(left<right && !isAlphaNumeric(s.charAt(left))){
                 left++;
             }
-            while(left<right&&!Character.isLetterOrDigit(s.charAt(right))){
+            while(left<right && !isAlphaNumeric(s.charAt(right))){
                 right--;
             }
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+            //compare the valid characters after converting both to the same case(lowercase)
+            if(toLower(s.charAt(left))!=toLower(s.charAt(right))){
                 return false;
             }
+            //move both pointers inward
             left++;
             right--;
 
         }
+        //if the pointer meet without finding a mismatch return true
         return true;
     }
 }
