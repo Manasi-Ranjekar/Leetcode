ed2951818cc6c795405819a352e7323e08ce25a6
Manasi-Ranjekar <manasi.ranjekar6@gmail.com>
2026-06-29 12:46:07 +0530

    Time: 1 ms (22.92%), Space: 48.1 MB (73.3%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,11 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
+        //initialize 2 pointers
         int right=s.length-1;
         int left=0;
+        //traverse the string till both pointers cross or meet each other
         while(left<right){
+            //swapping logic
             char temp=s[left];
             s[left]=s[right];
             s[right]=temp;
+            //update the pointers
             left++;
             right--;
         }
